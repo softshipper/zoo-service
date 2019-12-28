c9465a7e090922bf7c8992cd1d7f23d0018331ce
Developer <developer@sweetsoft.com>
2019-12-28 22:08:10 +0100

    Add routing path

--- src/main/java/com/sweetsoft/App.java
+++ src/main/java/com/sweetsoft/App.java
@@ -3,20 +3,36 @@ package com.sweetsoft;
 import io.reactivex.Single;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.core.http.HttpServer;
+import io.vertx.reactivex.core.http.HttpServerResponse;
+import io.vertx.reactivex.ext.web.Route;
+import io.vertx.reactivex.ext.web.Router;
 
 public class App {
 
   private static Vertx vertx = Vertx.vertx();
+  private static Router router = Router.router(vertx);
+
+  private static void buildGreetRoute(Router router) {
+
+    Route route = router.route().path("/greet");
+    route.handler(routingContext -> {
+
+      HttpServerResponse response = routingContext.response();
+      response.end("Greet from zoo service!!!");
+
+    });
+
+  }
 
 
   public static void main(String[] args) {
 
+    buildGreetRoute(router);
+
     Single<HttpServer> single = vertx
       .createHttpServer()
-      .requestHandler(req -> {
-        req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!");
-      })
-      .rxListen(80, "0.0.0.0");
+      .requestHandler(router)
+      .rxListen(8080, "0.0.0.0");
 
     single.
       subscribe(
